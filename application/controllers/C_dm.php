8edc590bcbe67856c4a44b9539a719ae97c22345
zien <sheenazien08@gmail.com>
2019-05-23 19:38:45 +0700

    pdf

--- application/controllers/C_dm.php
+++ application/controllers/C_dm.php
@@ -21,7 +21,18 @@ class C_dm extends CI_Controller
 
     public function ambil($id)
     {
-        dd($id);
+        $this->load->library('PdfGenerator');
+
+        $data['users']=array(
+            array('firstname'=>'Agung','lastname'=>'Setiawan','email'=>'ag@setiawan.com'),
+            array('firstname'=>'Hauril','lastname'=>'Maulida Nisfar','email'=>'hm@setiawan.com'),
+            array('firstname'=>'Akhtar','lastname'=>'Setiawan','email'=>'akh@setiawan.com'),
+            array('firstname'=>'Gitarja','lastname'=>'Setiawan','email'=>'git@setiawan.com')
+        );
+
+        $html = $this->load->view('v_prodi/pdfskpi', $data, true);
+
+        $this->pdfgenerator->generate($html, 'contoh');
     }
 }
 
